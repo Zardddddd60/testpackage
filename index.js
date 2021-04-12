c662b4e483365da2f1aecb5392cc6185dc10b711
竹釉 <ld233998@alibaba-inc.com>
2021-04-12 20:40:27 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.fn = void 0;
+function fn() {
+    console.log('asd');
+}
+exports.fn = fn;
+//# sourceMappingURL=index.js.map
